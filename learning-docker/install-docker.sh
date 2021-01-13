240712ca0d90e8c067756ad3a9576db1b9c057a6
Brian Linkletter <mail@brianlinkletter.ca>
2021-01-12 20:09:52 -0500

    Firts course

---
+++ learning-docker/install-docker.sh
@@ -0,0 +1,40 @@
+# Install docker-ce on Ubuntu 20.04
+
+# If Docker is already installed...
+# sudo apt-get remove docker docker-engine docker.io containerd runc
+
+sudo apt-get update
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+sudo docker run hello-world
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+# Log out and then back in, or run the "newgrp" command
+
+newgrp docker
+
+sudo systemctl enable docker
+
+
+
+
+
