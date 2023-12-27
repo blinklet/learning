e9b0e5dea96bfb6d603b509ebb71e4b7afe9445e
Brian Linkletter <mail@brianlinkletter.ca>
2023-12-27 18:24:46 -0500

    learning

--- data-science/post3.py
+++ data-science/post3.py
@@ -4,14 +4,12 @@ import json
 url = "http://localhost:11434/api/generate"
 
 data = {
-    "model": "orca-mini:3b",
-    "system": "Always answer using ten or less words.",
-    "prompt": "Tell me why the sky is blue."
+    "model": "mistral:7b",
+    "prompt": "Using less than 10 words, tell me what color is the sky."
 }
 
 response = requests.post(url, json=data)
 
 data = response.text.splitlines()
 response_list = [json.loads(line)['response'] for line in data]
-
 print(''.join(response_list))
